0baa1d0d4bb4d1db1afa903d9f17f5db6267b695
user1 <user1@DESKTOP-H0PL0OB>
2024-05-30 17:10:41 +0900

    [INIT] 미니 프로젝트 연습물 1일차

---
+++ src/Practice/Info.java
@@ -0,0 +1,48 @@
+package Practice;
+
+
+public class Info {
+	private String date;
+	private String phone;
+	private	String Memberid;
+	private String Memberpw;
+	private String Membercard;
+	private String MemberEmail;
+public Info() {}
+	public String getMemberEmail() {
+		return MemberEmail;
+	}
+	public void setMemberEmail(String MemberEmail) {
+		this.MemberEmail = MemberEmail;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getMemberid() {
+		return Memberid;
+	}
+	public void setMemberid(String memberid) {
+		Memberid = memberid;
+	}
+	public String getMemberpw() {
+		return Memberpw;
+	}
+	public void setMemberpw(String memberpw) {
+		Memberpw = memberpw;
+	}
+	public String getMembercard() {
+		return Membercard;
+	}
+	public void setMembercard(String membercard) {
+		Membercard = membercard;
+	}
+}
---
+++ src/Practice/View.java
@@ -0,0 +1,104 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class View {
+	Scanner sc = new Scanner(System.in);
+
+	public Info menu1() {
+		System.out.println("===로그인===");
+		System.out.print("ID: ");
+		String Memberid = sc.next();
+		System.out.println("PW: ");
+		String Memberpw = sc.next();
+		Info info = new Info();
+		info.setMemberid(Memberid);
+		info.setMemberpw(Memberpw);
+		return info;
+	}
+	
+	public int menu() {
+
+		System.out.println("메뉴 선택");
+		System.out.println("1. 문의 관련");
+		System.out.println("2. 원격 제어");
+		System.out.println("3. 종료");
+
+		System.out.println("메뉴 선택(1~3): ");
+		int select = sc.nextInt();
+		return select;
+	}
+
+
+	public void enquire() { // 문의
+		System.out.println("1. 구매관련문의");
+		System.out.println("2. 오류관련문의");
+		System.out.println();
+		System.out.println("메뉴 선택(1~2): ");
+//		int input = sc.nextInt();
+//		return input;
+
+	}
+	public void purchase() { // 1-1번 구매
+		System.out.println("1.결제가 됬으나 미구매 상태");
+		System.out.println("2.결제 시 구매 창 진행 안됨");
+		System.out.println();
+		System.out.println("메뉴 선택 (1~2): ");
+//		int purchase = sc.nextInt();
+//		return purchase;
+	}
+	public Info purchase1() {
+		System.out.print("결제 카드 번호 입력: ");
+		String Membercard = sc.next();
+		System.out.print("이메일 입력: ");
+		String MemberEmail = sc.next();
+	
+		Info info1 = new Info();
+		info1.setMembercard(Membercard);
+		info1.setMemberEmail(MemberEmail);
+		return info1;
+	}
+	public void purchase2() {
+		System.out.println("호환성 추가 및 새로고침 진행 권장");
+	}
+	
+	public void error() { // 1-2번 오류
+
+		System.out.println("1.사이트 접속 안됨");
+		System.out.println("2.로그인이 안됨");
+		System.out.println();
+		System.out.println("메뉴 선택 (1~2): ");
+//		int error = sc.nextInt();
+//		return error;
+	}
+
+	public void error1() {
+		System.out.println("호환성 추가 및 새로고침 권장");
+	}
+
+	public void error2() {
+		System.out.println("키보드 관련 보안 프로그램 재설치 권장");
+	}
+	
+	public void control() {// 2번 원격
+
+		System.out.println("1. 원격 제어 신청");
+//		int num = sc.nextInt();
+//		return num;
+	}
+	
+	public Info control1() {
+		System.out.print("날짜 입력: ");
+		String date  = sc.next();
+		System.out.println("이메일 입력: ");
+		String MemberEmail = sc.next();
+		
+		Info info2 = new Info();
+		info2.setDate(date);
+		info2.setMemberEmail(MemberEmail);
+		return info2;
+	}
+	public void printMessage(String msg) {
+		System.out.println(msg);
+	}
+}
